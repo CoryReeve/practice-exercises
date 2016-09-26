4bc2a03223f326a4306b65150665db9b1af9fb87
Cory <cory.reeve@gmail.com>
2016-09-25 23:19:20 -0400

    - Expanded Unit Tests - Added decimal rounding functions from MDN

--- tip-calculator/basic-js/tests.js
+++ tip-calculator/basic-js/tests.js
@@ -6,9 +6,26 @@ describe("#calculateTipAmount", function () {
 
     it("should take a billAmount and tipPercent and return a tipAmount", function () {
         var tipAmount = calculateTipAmount(100, 25);
-        expect(tipAmount).toBe(25);
+        expect(tipAmount).toBe(25.00);
 
         tipAmount = calculateTipAmount(50, 20);
-        expect(tipAmount).toBe(10);
+        expect(tipAmount).toBe(10.00);
     });
+
+    it("should round numbers off to two decimal places", function () {
+        var tipAmount = calculateTipAmount(6.23, 21);
+        expect(tipAmount).toBe(1.31);
+
+        tipAmount = calculateTipAmount(2.23, 20);
+        expect(tipAmount).toBe(.45);
+    });
+
+});
+
+describe("#calculateTip", function () {
+
+    it("should be defined", function () {
+        expect(calculateTip).toBeDefined();
+    });
+
 });
